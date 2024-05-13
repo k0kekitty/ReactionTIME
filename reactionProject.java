ff10cce0ed05c9874d8d293e175de828b083de9b
k0kekitty <mariacymbalyuk@gmail.com>
2024-05-13 19:29:42 -0400

    Basic Code
    Created base code for the reaction time project.

--- reactionProject.java
+++ reactionProject.java
@@ -1,1 +1,28 @@
+import javafx.application.Application;
+import javafx.scene.layout.StackPane;
+import javafx.scene.shape.Rectangle;
+import javafx.scene.input.KeyEvent;
+import javafx.scene.input.KeyCode;
+import javafx.stage.Stage;
+import javafx.scene.layout.Pane;
+import javafx.scene.Scene;
 
+
+public class reactionProject extends Application {
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+    StackPane pane = new StackPane();
+
+
+
+
+    Scene scene = new Scene(pane,1000,700);
+    primaryStage.setTitle("Reaction Time");
+    primaryStage.setScene(scene);
+    primaryStage.show();
+ }
+
+    public static void main(String[] args) throws Exception{
+        launch(args);
+ }
+}
