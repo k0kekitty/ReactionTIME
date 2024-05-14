0c1323fe3c843c47211e2ca4f08544de9632c09c
k0kekitty <mariacymbalyuk@gmail.com>
2024-05-14 14:15:30 -0400

    Added start "screen" for rect
    Added start button so that the reaction "game" can begin. Red rect appears afterwards.

--- reactionProject.java
+++ reactionProject.java
@@ -8,36 +8,71 @@ import javafx.scene.layout.Pane;
 import javafx.scene.Scene;
 import javafx.scene.paint.Color;
 import javafx.event.EventHandler;
+import javafx.scene.control.Button;
+import javafx.event.ActionEvent;
 
 
 public class reactionProject extends Application {
+    private boolean start = true;
+    private Rectangle redRect = new Rectangle(0,0,300,150);
+    private Button startButt = new Button("Start");
+    private StackPane pane = new StackPane();
+    private Scene scene = new Scene(pane,1000,700);
+
     @Override
     public void start(Stage primaryStage) throws Exception {
-    StackPane pane = new StackPane();
-    Rectangle redRect = new Rectangle(0,0,300,150);
-    redRect.setFill(Color.RED);
-    pane.getChildren().add(redRect);
 
+        /* 
+        redRect.setFill(Color.RED);
+        pane.getChildren().add(redRect);
+         */
+        pane.getChildren().add(startButt);
+        
+        startButt.setOnAction(new startHandler());
 
+        //Scene scene = new Scene(pane,1000,700);
 
-    Scene scene = new Scene(pane,1000,700);
-
-
-
-    scene.setOnKeyPressed(new EventHandler<KeyEvent>() {
-        @Override
-        public void handle(KeyEvent event) {
-            if (event.getCode() == KeyCode.SPACE) {
+        /* 
+        scene.setOnKeyPressed(new EventHandler<KeyEvent>() {
+            @Override
+            public void handle(KeyEvent event) {
+                if (event.getCode() == KeyCode.SPACE) {
                 pane.getChildren().clear();
             }
-        }
-    });
+            }
+        });
+        */
 
-    
-    primaryStage.setTitle("Reaction Time");
-    primaryStage.setScene(scene);
-    primaryStage.show();
+        primaryStage.setTitle("Reaction Time");
+        primaryStage.setScene(scene);
+        primaryStage.show();
  }
+
+    class startHandler implements EventHandler<ActionEvent> {
+        @Override
+        public void handle(ActionEvent e) {
+            beginGame();
+        }
+    }
+
+    private void beginGame() {
+        start = false;
+        pane.getChildren().remove(startButt);
+        redRect.setFill(Color.RED);
+        pane.getChildren().add(redRect);
+        //for (int i = 0; i < 6; i++) {
+            //pane.getChildren().add(redRect);
+        //}
+
+        scene.setOnKeyPressed(new EventHandler<KeyEvent>() {
+            @Override
+            public void handle(KeyEvent event) {
+                if (event.getCode() == KeyCode.SPACE) {
+                pane.getChildren().clear();
+            }
+            }
+        });
+    }
 
     public static void main(String[] args) throws Exception{
         launch(args);
