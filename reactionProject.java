62886ae2528f87be5af69e4bd1499fd595652a94
k0kekitty <mariacymbalyuk@gmail.com>
2024-05-24 20:44:46 -0400

    Cleaning up
    Removed old code; took away whitespace; made it look better overall

--- reactionProject.java
+++ reactionProject.java
@@ -37,33 +37,13 @@ public class reactionProject extends Application {
     private long startTime = 0;
     private long finish = 0;
     private long estimatedTime = 0;
-    
-    
 
     @Override
     public void start(Stage primaryStage) throws Exception {
-
-        /* 
-        redRect.setFill(Color.RED);
-        pane.getChildren().add(redRect);
-         */
         description.setContentDisplay(ContentDisplay.BOTTOM);
         description.setTextFill(Color.RED);
         pane.getChildren().add(description);
         startButt.setOnAction(new startHandler());
-
-        //Scene scene = new Scene(pane,1000,700);
-
-        /* 
-        scene.setOnKeyPressed(new EventHandler<KeyEvent>() {
-            @Override
-            public void handle(KeyEvent event) {
-                if (event.getCode() == KeyCode.SPACE) {
-                pane.getChildren().clear();
-            }
-            }
-        });
-        */
 
         primaryStage.setTitle("Reaction Time");
         primaryStage.setScene(scene);
@@ -81,131 +61,17 @@ public class reactionProject extends Application {
         start = true;
         pane.getChildren().remove(description);
         rectOnScreen = false;
-        /*
-        EventHandler<KeyEvent> spaceHandler = e -> {
-                if(event.getCode() == KeyCode.SPACE && count < 6) {
-                    if (rectOnScreen == true) {
-                        pane.getChildren().clear();
-                        rectOnScreen = false;
-                    } else if (rectOnScreen == false) {
-                        pane.getChildren().add(redRect);
-                        rectOnScreen = true;
-                    }
-                }
-        };
-        */
-
-
-
-
-        /* 
-        Timeline animation = new Timeline();
-        animation.setCycleCount(Timeline.INDEFINITE);
-
-        animation.getKeyFrames().addAll(new KeyFrame(Duration.millis(randomInt), new EventHandler<ActionEvent>() {
-            @Override
-            public void handle(ActionEvent event) {
-                if (rectOnScreen == false) {
-                    pane.getChildren().add(redRect);
-                    int andomInt = generator.nextInt(1000) + 5;
-                    animation.setRate(1d/andomInt);
-                    rectOnScreen = true;
-                } else {
-                    pane.getChildren().clear();
-                    int andomInt = generator.nextInt(1000) + 5;
-                    animation.setRate(1d/andomInt);
-                    rectOnScreen = false;
-                }
-            }
-        }));
-        animation.play();
-
-        scene.setOnKeyPressed(new EventHandler<KeyEvent>() {
-            @Override
-            public void handle(KeyEvent event) {
-                if(event.getCode() == KeyCode.SPACE && count < 6) {
-                    if (animation.getStatus() == Animation.Status.RUNNING) {
-                        //pane.getChildren().clear();
-                        animation.pause();
-                        int andomInt = generator.nextInt(1) + 5;
-                        animation.setRate(1d/andomInt);
-                    } else if (animation.getStatus() == Animation.Status.PAUSED) {
-                        animation.play();
-                        int andomInt = generator.nextInt(1) + 5;
-                        animation.setRate(1d/andomInt);
-                        //pane.getChildren().add(redRect);
-                        //rectOnScreen = true;
-                    }
-                }
-            }
-        });
-       */
-
-
-        //just flashes
-        /* 
-        EventHandler<ActionEvent> eventHandler = e ->{
-            if (rectOnScreen == false) {
-                pane.getChildren().add(redRect);
-                rectOnScreen = true;
-            } else {
-                pane.getChildren().clear();
-                rectOnScreen = false;
-            }
-        };
-
-        Timeline animation = new Timeline (new KeyFrame(Duration.millis(randomInt), eventHandler));
-        animation.setCycleCount(Timeline.INDEFINITE);
-        animation.play();
-
-        scene.setOnKeyPressed(new EventHandler<KeyEvent>() {
-            @Override
-            public void handle(KeyEvent event) {
-                if(event.getCode() == KeyCode.SPACE && count < 6) {
-                    if (animation.getStatus() == Animation.Status.RUNNING) {
-                        //pane.getChildren().clear();
-                        animation.pause();
-                        int andomInt = generator.nextInt(1) + 5;
-                        animation.setRate(1d/andomInt);
-                    } else if (animation.getStatus() == Animation.Status.PAUSED) {
-                        animation.play();
-                        int andomInt = generator.nextInt(1) + 5;
-                        animation.setRate(1d/andomInt);
-                        //pane.getChildren().add(redRect);
-                        //rectOnScreen = true;
-                    }
-                }
-            }
-        });
-        */
-
-        /* 
-        scene.setOnKeyPressed(new EventHandler<KeyEvent>() {
-            @Override
-            public void handle(KeyEvent event) {
-                if(event.getCode() == KeyCode.SPACE && count < 6) {
-                    if (rectOnScreen == true) {
-                        pane.getChildren().clear();
-                        rectOnScreen = false;
-                    } else if (rectOnScreen == false) {
-                        pane.getChildren().add(redRect);
-                        rectOnScreen = true;
-                    }
-                }
-            }
-        });
-        */
 
         redRect.setFill(Color.RED);
 
         EventHandler<ActionEvent> eventHandler = e -> {
             if (rectOnScreen == false) {
+                //long startTimeforRect = System.nanoTime();
                 pane.getChildren().add(redRect);
+                //long endTimeForRect = System.nanoTime();
+                //System.out.println("Time for rectangle to appear: " + (endTimeForRect - startTimeforRect));
                 startTime = System.currentTimeMillis();
                 rectOnScreen = true;
-            } else {
-                //pane.getChildren().clear();
-                //rectOnScreen = false;
             }
         };
 
@@ -213,46 +79,21 @@ public class reactionProject extends Application {
         animation.setCycleCount(Timeline.INDEFINITE);
 
         animation.play();
-        /* 
-        if(rectOnScreen == true) {
-            //startTime = System.nanoTime();
-            //animation.pause();
-            //startTime = System.nanoTime();
-            //startTime = System.currentTimeMillis();
-            //gettingReactionTime(animation);
-        } else {
-            animation.play();
-        }
-        */
-        /* 
-        if (animation.getStatus() == Animation.Status.PAUSED) {
-            System.out.println("HI");
-        } else {
-            System.out.println("bye");
-        }
-         */
 
-        
         scene.setOnKeyPressed(new EventHandler<KeyEvent>() {
             @Override
             public void handle(KeyEvent event) {
                 if(event.getCode() == KeyCode.SPACE && count < 6) {
                     if (rectOnScreen == true) {
-                        //finish = System.nanoTime();
                         finish = System.currentTimeMillis();
                         rectOnScreen = false;
-                        //finish = finish/1000000000;
                         estimatedTime = finish-startTime;
                         totalTime += estimatedTime;
                         count++;
                         pane.getChildren().clear();
                         System.out.println("The start time is: " + startTime);
                         System.out.println("The finish time is: " + finish);
-                        //System.out.println("The estimated time is: " + estimatedTime);
-                        //System.out.println("The total time is: " + totalTime);
-                        //System.out.println("The count is: " + count);
                         System.out.println();
-                        //rectOnScreen = false;
                     } else if (rectOnScreen == false) {
                         System.out.println("The rectangle is not on the screen.");
                     }
@@ -266,15 +107,7 @@ public class reactionProject extends Application {
         });
     }
 
-    /* 
-    private void gettingReactionTime(Timeline animation1) {
-        System.out.println("hi");
-        //animation1.pause();
-        System.out.println("hi");
-    }
-    */
-
-    public static void main(String[] args) throws Exception{
+    public static void main(String[] args) throws Exception {
         launch(args);
  }
 }
